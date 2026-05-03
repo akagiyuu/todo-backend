0a4e045ebdf8cb3bb53b20a41516c7da4fc56f43
akagiyuu <khanghuynh0245@gmail.com>
2026-05-03 13:58:53 +0700

    feat(http): add handler struct

---
+++ internal/transports/http/handler.go
@@ -0,0 +1,27 @@
+package http
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/go-fuego/fuego"
+	"github.com/go-fuego/fuego/option"
+	httpSwagger "github.com/swaggo/http-swagger/v2"
+)
+
+func (s *Server) RegisterRoutes(f *fuego.Server) {
+	fuego.Get(f, "/", s.PingHandler)
+}
+
+func (s *Server) OpenAPIHandler(specURL string) http.Handler {
+	return httpSwagger.Handler(
+		httpSwagger.Layout(httpSwagger.StandaloneLayout),
+		httpSwagger.PersistAuthorization(true),
+		httpSwagger.URL(specURL),
+	)
+}
+
+func (s *Server) PingHandler(c fuego.ContextNoBody) (string, error) {
+	return "pong", nil
+}
